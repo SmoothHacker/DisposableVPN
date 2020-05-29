ed25cc1f84d965f168e394d0adabd333052648ac
Scott Lagler <laglerscott@gmail.com>
2020-05-28 23:41:01 -0500

    Create feature to create and destroy droplet

--- DisposableVPN.py
+++ DisposableVPN.py
@@ -1,5 +1,52 @@
-import digitalocean
 from DropletManager import DropletManager
+import sys
 
-do = DropletManager('')
-do.print_account_info()
+
+def load_api_token():
+    token_file = open("token.env")
+    return token_file.read()
+
+
+VPN_droplet = DropletManager(load_api_token())
+
+if not VPN_droplet.check_credentials():
+    print("Please check the token.env file")
+    sys.exit(1)
+
+while True:
+    print(26 * "-", "DisposableVPN", 26 * "-")
+    print("1. Create Droplet")
+    print("2. Exit")
+    print(67 * "-")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("Select Region")
+        for x in VPN_droplet.regions:
+            print(x, end=" ")
+
+        regionChoice = input("Enter your region choice: ")
+        VPN_droplet.create_server(regionChoice)
+    elif choice == 2:
+        print("Now Quitting .....")
+        break
+    else:
+        input("Please Use numerical values. Enter any key to try again..")
+
+    # Display status about server config
+
+    while True:
+        # Display menu about destroying server
+        print("\n", 26 * "-", "DisposableVPN", 26 * "-")
+        print("Droplet is Live! in ", regionChoice)
+        print("External IP: ", VPN_droplet.ip, "\n")
+        print("1. Destroy Droplet")
+        print(67 * "-")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            VPN_droplet.delete_server()
+            break
+        else:
+            input("Please Use numerical values. Enter any key to try again..")
--- DropletManager.py
+++ DropletManager.py
@@ -1,11 +1,14 @@
 import digitalocean
+import time
 
 
 class DropletManager:
 
     def __init__(self, api_token):
+        self.regions = ["NYC1", "NYC3", "AMS3", "SFO2", "SFO3", "SGP1", "LON1", "FRA1", "TOR1", "BLR1"]
         self.manager = digitalocean.Manager(token=api_token)
         self.Droplet = digitalocean.Droplet
+        self.ip = 0
 
     # def import_cloud_init(self):
     def print_account_info(self):
@@ -14,9 +17,9 @@ class DropletManager:
         print("Account Status: ", self.manager.get_account().status)
 
     def check_credentials(self):
-        try: # Could be a better way
-            if self.manager.get_account().status == "Active":
-                print("API Key is valid and Account is Active")
+        try:  # Could be a better way
+            if self.manager.get_account().status == "active":
+                print("API Key is valid and Account is Active\n")
                 return True
             else:
                 print("Account Status is inactive")
@@ -25,6 +28,12 @@ class DropletManager:
             print("API Key is invalid")
             return False
 
-    #def create_server(self):
+    def create_server(self, region):
+        self.Droplet = digitalocean.Droplet(token=self.manager.token, name='VPNdroplet', region=region.lower(), image='ubuntu-20-04-x64', size_slug='s-1vcpu-1gb', user_data="")
+        self.Droplet.create()
+        time.sleep(30)
+        self.ip = self.Droplet.load().ip_address
 
-    #def delete_server(self):
+    def delete_server(self):
+        self.Droplet.destroy()
+        print("Droplet Destroyed")
