860ea13a72e3809122bf550372fc0896fb2163db
SmoothHacker <laglerscott@gmail.com>
2020-06-04 12:03:30 -0500

    Fixed DNS issue
    Will have to create a socket system to send a message when the server is ready

--- DropletManager.py
+++ DropletManager.py
@@ -10,19 +10,14 @@ class DropletManager:
         self.publickey_server = open(os.path.join(os.getcwd(), "keys", "publickey_server")).read()
         self.privatekey_client = open(os.path.join(os.getcwd(), "keys", "privatekey_client")).read()
         self.privatekey_server = open(os.path.join(os.getcwd(), "keys", "privatekey_server")).read()
-        self.regions = {1: "NYC1", 2: "NYC3", 3: "AMS3", 4: "SFO2", 5: "SFO3", 6: "SGP1", 7: "LON1", 8: "FRA1", 9: "TOR1", 10: "BLR1"}
+        self.regions = {"1": "NYC1", "2": "NYC3", "3": "AMS3", "4": "SFO2", "5": "SFO3", "6": "SGP1", "7": "LON1",
+                        "8": "FRA1", "9": "TOR1", "10": "BLR1"}
         self.manager = digitalocean.Manager(token=api_token)
         self.Droplet = digitalocean.Droplet
         self.ip = None
         self.user_data = None
         # Will be added via a generation function
-        self.ssh_pub_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6tkXKWck9CbTRDBVPKDNHqktSfDI05tInttlupUfd0difQM8mMZ36/gyQE3XZlyXv7YomN/ErSJB8LovqWms44FxY6JCKolgvSeUG/2f3pq08hTcZ1qdtp7MYI0C/t8aseP+0zinepMNaRw1Ta60h2Q+4gDQD2a/dFWbaYGPVruTvmEz5P9wVoIVWiGyDM52NE1fALLktOoRWgP12AimxcAtw8Dt5Exr+0alNXY4hBSpxpPf442Fal61BxSr3Xr6zdb5X2vpcvKuiUSww7GVv5q8fpJT9kUq5+XZdmJEvVCLdWe667PI6re/ClTekTOQZsj0Cb8Dr4XXzcU9zawzsHE4+CnKQKwDmWIz2FMAS7rqp+r2v+Umx1l+CNzXksKXtW/Px6j3kPuMVjZRxZ2ZcqAJdbbpqrN2FsNxwkOkp+4RxWmno4QNkj9hvgEjJx3aQgm6suh1Ajp6smGLbfAPh0LDNWTD9xSuk5ULYuIfguiPmxH4JXKRVO3f/LlGrZWs= DisposableVPN"
-        if not self.manager.get_all_sshkeys().__contains__(self.ssh_pub_key):
-            digitalocean.SSHKey(token=api_token, name="DisposableVPN", public_key=self.ssh_pub_key).create()
-
-    def import_wireguard_script(self):
-        with open("wireguard_setup.sh", 'r') as file:
-            self.user_data = file.read().replace("\n", ' ')
+        # self.ssh_pub_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6tkXKWck9CbTRDBVPKDNHqktSfDI05tInttlupUfd0difQM8mMZ36/gyQE3XZlyXv7YomN/ErSJB8LovqWms44FxY6JCKolgvSeUG/2f3pq08hTcZ1qdtp7MYI0C/t8aseP+0zinepMNaRw1Ta60h2Q+4gDQD2a/dFWbaYGPVruTvmEz5P9wVoIVWiGyDM52NE1fALLktOoRWgP12AimxcAtw8Dt5Exr+0alNXY4hBSpxpPf442Fal61BxSr3Xr6zdb5X2vpcvKuiUSww7GVv5q8fpJT9kUq5+XZdmJEvVCLdWe667PI6re/ClTekTOQZsj0Cb8Dr4XXzcU9zawzsHE4+CnKQKwDmWIz2FMAS7rqp+r2v+Umx1l+CNzXksKXtW/Px6j3kPuMVjZRxZ2ZcqAJdbbpqrN2FsNxwkOkp+4RxWmno4QNkj9hvgEjJx3aQgm6suh1Ajp6smGLbfAPh0LDNWTD9xSuk5ULYuIfguiPmxH4JXKRVO3f/LlGrZWs= DisposableVPN"
 
     def print_account_info(self):
         print("Account Info: ")
@@ -33,7 +28,7 @@ class DropletManager:
         try:  # Could be a better way
             if self.manager.get_account().status == "active":
                 # Send SSH Key
-                print("API Key is valid and Account is Active\n")
+                print("API Key is Valid and Account is Active\n")
                 return True
             else:
                 print("Account Status is inactive")
@@ -43,17 +38,18 @@ class DropletManager:
             return False
 
     def create_server(self, region_choice):
-        self.create_wireguard_configs()
+        self.create_server_script()
         self.Droplet = digitalocean.Droplet(token=self.manager.token,
                                             name='VPNdroplet',
                                             region=self.regions[region_choice],
                                             image='ubuntu-20-04-x64',
                                             size_slug='s-1vcpu-1gb',
-                                            user_data=self.user_data,
-                                            ssh_key=self.ssh_pub_key)
+                                            user_data=self.user_data)
         self.Droplet.create()
-        time.sleep(20)
+        print("Droplet is being constructed . . . . .")
+        time.sleep(60)
         self.ip = self.Droplet.load().ip_address
+        self.create_client_config()
 
     def delete_server(self):
         self.Droplet.destroy()
@@ -67,12 +63,12 @@ class DropletManager:
         self.privatekey_client = open(os.path.join(os.getcwd(), "keys", "privatekey_client")).read()
         self.privatekey_server = open(os.path.join(os.getcwd(), "keys", "privatekey_server")).read()
 
-    def create_wireguard_configs(self):
+    def create_client_config(self):
         client_config_file = open("wg_client.conf", "w")
         client_config = f"""[Interface]
 Address = 10.0.0.2/32
 PrivateKey = {self.privatekey_client}
-DNS = 1.1.1.1
+DNS = 8.8.8.8
 
 [Peer]
 PublicKey = {self.publickey_server}
@@ -82,20 +78,21 @@ AllowedIPs = 0.0.0.0/0
         client_config_file.write(client_config)
         client_config_file.close()
 
-        server_config_file = open("wireguard_setup.sh", "w")
-        server_config = f"""#!/bin/bash
+    def create_server_script(self):
+        self.user_data = f"""#!/bin/bash
 apt update && apt install wireguard -y
-echo '
-  [Interface]
-  PrivateKey = {self.privatekey_server}
-  Address = 10.0.0.1/24
-  ListenPort = 51820
+echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+sysctl -p
+echo '[Interface]
+PrivateKey = {self.privatekey_server}
+Address = 10.0.0.1/24
+ListenPort = 51820
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 
-  [Peer]
-  PublicKey = {self.publickey_client}
-  AllowedIPs = 10.0.0.2/32
-  ' > /etc/wireguard/wg0.conf
-cd /etc/wireguard/
-wg-quick up wg0.conf"""
-        server_config_file.write(server_config)
-        server_config_file.close()
+[Peer]
+PublicKey = {self.publickey_client}
+AllowedIPs = 10.0.0.2/32
+' > /etc/wireguard/wg0.conf
+
+wg-quick up wg0"""
